942f05003d7950b5c3a687d043404b7bc34bf483
Ed Rogers <etfrogers@hotmail.com>
2023-05-07 11:23:36 +0100

    Allow example_authenticate to listen on port 5000 for url, rather than paste it back

--- example_authenticate.py
+++ example_authenticate.py
@@ -1,5 +1,7 @@
+import datetime
 import json
 import logging
+import socket
 import urllib.parse
 
 import msal
@@ -9,6 +11,29 @@ from pyzehndercloud.auth import OAUTH2_AUTHORITY, OAUTH2_REDIRECT_URL
 
 _LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
+
+HOST = 'localhost'
+PORT = 5000
+
+
+def listen_on_socket():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        received = b''
+        start_time = datetime.datetime.now()
+        timeout = start_time + datetime.timedelta(seconds=5)
+        with conn:
+            print(f"Connected by {addr}")
+            while datetime.datetime.now() < timeout:
+                data = conn.recv(1024)
+                print(data)
+                if not data:
+                    break
+                received += data
+    return received
+
 
 def run():
     # Create the msal app
@@ -24,13 +49,14 @@ def run():
     print(flow["auth_uri"])
     print()
 
-    # Parse the reply
-    print("Enter the URL you were redirected to, but don't continue on that page:")
-    url = input()
+    request_data = listen_on_socket()
+    request_str = request_data.decode("utf-8")
+    get, url, *tokens = request_str.split()
+
     query_params = dict(urllib.parse.parse_qsl(urllib.parse.urlparse(url).query))
     if 'client_info' in query_params and query_params.get('client_info') == '1':
         del query_params['client_info']
-    result = msal_app.acquire_token_by_auth_code_flow(flow, query_params) # , scopes=['df77b1ce-c368-4f7f-b0e6-c1406ac6bac9']
+    result = msal_app.acquire_token_by_auth_code_flow(flow, query_params)
 
     if result.get("error"):
         print(
