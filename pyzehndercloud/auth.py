7a132149b5e1e48139a392d44d4a3f47b04d7a6d
Ed Rogers <etfrogers@hotmail.com>
2023-05-15 11:26:10 +0100

    Move InteractiveAuth into the pyzehndercloud core, rather than example

--- pyzehndercloud/auth.py
+++ pyzehndercloud/auth.py
@@ -1,6 +1,11 @@
+import atexit
+import os
 from abc import ABC, abstractmethod
 
+import msal
 from aiohttp import ClientSession
+
+from example import logger
 
 TENANT = 'zehndergroupauth'
 POLICY = 'B2C_1_signin_signup_enduser'
@@ -29,3 +34,55 @@ class AbstractAuth(ABC):
     @abstractmethod
     async def async_get_access_token(self) -> str:
         """Return a valid access token."""
+
+
+class InteractiveAuth(AbstractAuth):
+    """This is an  implementation of the AbstractAuth class that uses pymsal's interactive auth flow"""
+
+    def __init__(self, websession: ClientSession, api_key: str, username: str):
+        super().__init__(websession, api_key)
+        self.username = username
+        cache = msal.SerializableTokenCache()
+        if os.path.exists("my_cache.json"):
+            cache.deserialize(open("my_cache.json", "r").read())
+        atexit.register(lambda:
+                        open("my_cache.json", "w").write(cache.serialize())
+                        # Hint: The following optional line persists only when state changed
+                        if cache.has_state_changed else None
+                        )
+        self.app = msal.PublicClientApplication(
+            OAUTH2_CLIENT_ID, authority=OAUTH2_AUTHORITY,
+            exclude_scopes=["profile"],
+            token_cache=cache,
+        )
+
+    async def async_get_access_token(self) -> str:
+        """Returns a token that can be used to authenticate against the API.
+        Note that this is an example, you probably want to cache the access_token, and only refresh it when it
+        expires.
+        """
+        accounts = self.app.get_accounts(username=self.username)
+        result = None
+        if accounts:
+            logger.info("Account(s) exists in cache, probably with token too. Let's try.")
+            logger.info("Account(s) already signed in:")
+            for a in accounts:
+                logger.debug(a["username"])
+            chosen = accounts[0]  # Assuming the end user chose this one to proceed
+            logger.info("Proceed with account: %s" % chosen["username"])
+            # Now let's try to find a token in cache for this account
+            result = self.app.acquire_token_silent([OAUTH2_CLIENT_ID], account=chosen)
+
+        if not result:
+            result = self.app.acquire_token_interactive(
+                [OAUTH2_CLIENT_ID],
+                port=OAUTH2_PORT,
+                # login_hint=config.get("username"),  # Optional.
+                # If you know the username ahead of time, this parameter can pre-fill
+                # the username (or email address) field of the sign-in page for the user,
+                # Often, apps use this parameter during reauthentication,
+                # after already extracting the username from an earlier sign-in
+                # by using the preferred_username claim from returned id_token_claims.
+                timeout=30,
+            )
+        return result['id_token']
